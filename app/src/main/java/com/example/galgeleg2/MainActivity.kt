5fddb6b12c19baed06f0fbbfdd592fb13dca84c3
fverne <frederikverne95@gmail.com>
2020-11-01 20:15:08 +0100

    progress added

--- app/src/main/java/com/example/galgeleg2/MainActivity.kt
+++ app/src/main/java/com/example/galgeleg2/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.galgeleg2
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,43 +12,62 @@ import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnKeyListener {
     private lateinit var txtinput: TextView
-    private lateinit var btn: Button
+    private lateinit var livestxt: TextView
     private lateinit var debugtxt: TextView
     private lateinit var debugtxt2: TextView
+    private lateinit var galgeimg: ImageView
     private val galgelogik: Galgelogik = Galgelogik()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        galgeimg = findViewById(R.id.galgeimg)
+        galgeimg.setImageResource(R.drawable.galge)
+
+        txtinput = findViewById(R.id.guessinput)
+        txtinput.setOnKeyListener(this)
+
+        livestxt = findViewById(R.id.lives)
+        livestxt.text = "Du har 6 liv før du taber."
+
         debugtxt = findViewById(R.id.test)
-        debugtxt.text = null
 
         debugtxt2 = findViewById(R.id.test2)
         debugtxt2.text = galgelogik.synligtOrd
 
-        txtinput = findViewById(R.id.guessinput)
-        txtinput.setOnKeyListener(this)
     }
 
     override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
-        // If the event is a key-down event on the "enter" button
+
+        // Changes enter button on keyboard to execute following code.
         if (event.action == KeyEvent.ACTION_DOWN &&
             keyCode == KeyEvent.KEYCODE_ENTER
         ) {
-            // Perform action on key press
             if (txtinput.length() == 1) {
                 galgelogik.gætBogstav(txtinput.text.toString().decapitalize(Locale.getDefault()))
 
                 if (galgelogik.erSidsteBogstavKorrekt()) {
-                    debugtxt.text = galgelogik.brugteBogstaver.toString()
                     debugtxt2.text = galgelogik.synligtOrd
                     Toast.makeText(this, "Dit gæt var korrekt!", Toast.LENGTH_LONG).show()
                 } else {
-                    debugtxt.text = galgelogik.brugteBogstaver.toString()
+                    livestxt.text = "Du har " + (6-galgelogik.antalForkerteBogstaver) + " liv tilbage."
                     Toast.makeText(this, "Dit gæt var forkert...", Toast.LENGTH_LONG).show()
+
+                    // switch case that changes the image
+                    when(galgelogik.antalForkerteBogstaver) {
+                        0 -> galgeimg.setImageResource(R.drawable.galge)
+                        1 -> galgeimg.setImageResource(R.drawable.forkert1)
+                        2 -> galgeimg.setImageResource(R.drawable.forkert2)
+                        3 -> galgeimg.setImageResource(R.drawable.forkert3)
+                        4 -> galgeimg.setImageResource(R.drawable.forkert4)
+                        5 -> galgeimg.setImageResource(R.drawable.forkert5)
+                        else -> galgeimg.setImageResource(R.drawable.forkert6)
+                    }
+
                 }
 
+                debugtxt.text = "Brugte Bogstaver: " + galgelogik.brugteBogstaver.toString()
                 txtinput.text = null
 
             }
